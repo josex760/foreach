a47d3fefea31b0967c56c0026df9ccdd29df85d4
josex760 <60684674+josex760@users.noreply.github.com>
2020-03-15 23:23:38 -0700

    Add files via upload

---
+++ foreach.php
@@ -0,0 +1,13 @@
+<?php
+//Loop
+
+//While loop
+//Do While loop
+//For loop
+//Foreach loop
+$array = array("Jose", "Rebecca", "Ramon", "Sadiel");
+
+foreach ($array as $loopdata) {
+	echo "My Name is ".$loopdata."<br>:";
+
+}
