523beccc029694e2303d933cfea65a37c99ed68a
josex760 <60684674+josex760@users.noreply.github.com>
2020-03-15 23:47:48 -0700

    Add files via upload

---
+++ phparrays.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$fruits = array("Bananas", "Apples", "Watermelons", "Cherries"); 
+echo "I like " . $fruits[0] . ", " . $fruits[1] . " and " . $fruits[2] .  $fruits[3] .".";
+?>
+
+</body>
+</html>
